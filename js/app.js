dea5bb41e82e421516a7af9beab5971cb4d12154
JK <jinhh999@gmail.com>
2017-02-27 17:14:07 +0800

    路由无缓存

--- js/app.js
+++ js/app.js
@@ -5,16 +5,14 @@
 
 	var myApp = angular.module('app', ['ngRoute', 'app.controllers.main']);
 
-
-	//设置路由+path值bug解决
+	//设置路由
 	myApp.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
 		$routeProvider
 		// /asdasda {status:asdasda}
-			.when('/:status', {
+			.when('/:status?', {
 				controller: 'MainController',
 				templateUrl: 'main_tmpl'
-			})
-			.otherwise({ redirectTo: '/' });
+			});
 		//处理自动前缀!#而造成路由器使用困难的bug==》改为了空字符串
 		$locationProvider.hashPrefix("");
 	}]);
